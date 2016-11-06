867aad66b62097d17004c4980209405145425de9
jelf <353742991@qq.com>
2016-11-06 00:27:33 -0400

    更新fire fox浏览器的聊天界面滚动条

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,11 +1,11 @@
 $(function() {
 	var websocket;
-	if ('WebSocket' in window) {
-		websocket = new WebSocket("ws://localhost:8080/SH/ws.do");
-	} else if ('MozWebSocket' in window) {
+	if('WebSocket' in window) {
+		websocket = new WebSocket("ws://localhost:8080/SHWS/ws.do");
+	} else if('MozWebSocket' in window) {
 		websocket = new MozWebSocket("ws://echo");
 	} else {
-		websocket = new SockJS("http://localhost:8080/SH/sockjs/ws.do");
+		websocket = new SockJS("http://localhost:8080/SHWS/sockjs/ws.do");
 	}
 	websocket.onopen = function(event) {
 		$("#tou").html("连接服务器成功!")
@@ -25,19 +25,20 @@ $(function() {
 	});
 
 	$('#message').keydown(function(e) {
-		if (e.keyCode == 13) {
+		if(e.keyCode == 13) {
 			send();
 		}
 	});
 	$('#message').focus();
+
 	function send() {
-		if ($('#message').val().trim() == "") {
+		if($('#message').val().trim() == "") {
 			return;
 		}
-		if (websocket != null) {
+		if(websocket != null) {
 			var data = JSON.stringify({
-				"toUserId" : "91e8319d-2bef-48f9-b857-2bd139379399",
-				"message" : $('#message').val().trim()
+				"toUserId": "91e8319d-2bef-48f9-b857-2bd139379399",
+				"message": $('#message').val().trim()
 			});
 			$('#message').val("");
 			websocket.send(data);
@@ -45,33 +46,36 @@ $(function() {
 			alert('未与服务器链接.');
 		}
 	}
+
 	function dealWithMessage(event) {
 		var data = jQuery.parseJSON(event.data);
 		var message = data.message.replace("&", "&amp;").replace("<", "&lt;");
 		var date = new Date(data.date).Format("hh:mm:ss");
 		console.log(message);
-		if (userId == data.userId) {
+		if(userId == data.userId) {
 			// 发送者
 			$("#chatBoxUl")
-					.append(
-							"<li><div class='sender'><div><input class='messageDataInput'data-messageid='cce8d038-e969-4a95-9e52-f1a9d17cb032'"
-									+ "type='hidden' data-time='"
-									+ date
-									+ "'><img src='img/headImage/b8cc5ae5-323d-4b22-a63c-ebab8041ae0d.png'></div><div>"
-									+ "<div class='left_triangle'></div><span>"
-									+ message + "</span></div></div></li>");
+				.append(
+					"<li><div class='sender'><div><input class='messageDataInput'data-messageid='cce8d038-e969-4a95-9e52-f1a9d17cb032'" +
+					"type='hidden' data-time='" +
+					date +
+					"'><img src='img/headImage/b8cc5ae5-323d-4b22-a63c-ebab8041ae0d.png'></div><div>" +
+					"<div class='left_triangle'></div><span>" +
+					message + "</span></div></div></li>");
 		} else {
 			$("#chatBoxUl")
-					.append(
-							"<li><div class='receiver'><div><input class='messageDataInput'data-messageid='cce8d038-e969-4a95-9e52-f1a9d17cb032'"
-									+ "type='hidden' data-time='"
-									+ date
-									+ "'><img src='img/headImage/b8cc5ae5-323d-4b22-a63c-ebab8041ae0d.png'></div><div>"
-									+ "<div class='right_triangle'></div><span>"
-									+ message + "</span></div></div></li>");
+				.append(
+					"<li><div class='receiver'><div><input class='messageDataInput'data-messageid='cce8d038-e969-4a95-9e52-f1a9d17cb032'" +
+					"type='hidden' data-time='" +
+					date +
+					"'><img src='img/headImage/b8cc5ae5-323d-4b22-a63c-ebab8041ae0d.png'></div><div>" +
+					"<div class='right_triangle'></div><span>" +
+					message + "</span></div></div></li>");
 		}
 		$("#chatBoxUl").stop().animate({
-			scrollTop : $('#chatBoxUl')[0].scrollHeight
-		}, 500);
+			scrollTop: $('#chatBoxUl')[0].scrollHeight
+		}, 500, function() {
+			$(this).mCustomScrollbar("update");
+		});
 	}
 });
